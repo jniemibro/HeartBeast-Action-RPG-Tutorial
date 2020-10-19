da69093b347e74e16fdd71cf26062043a81e6f85
Jonathan Niemi <jontniemi@icloud.com>
2020-10-19 12:34:50 -0400

    Completed Part 12
    -added roll

--- 2D Action RPG Project/Scripts/Player.gd
+++ 2D Action RPG Project/Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
 export var speed = 100
-const acceleration = 500
-const friction = 500
+
+const ROLL_SPEED = 100
+const ACCELERATION = 500
+const FRICTION = 500
 
 enum {
 	MOVE,
@@ -10,8 +12,11 @@ enum {
 	ATTACK
 }
 
+const START_VECTOR = Vector2.DOWN
+
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = START_VECTOR
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -20,16 +25,17 @@ onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	animationTree.active = true
 	# match default blend pos for all anim states on start
-	animationTree.set("parameters/Idle/blend_position", Vector2.ZERO)
-	animationTree.set("parameters/Run/blend_position", Vector2.ZERO)
-	animationTree.set("parameters/Attack/blend_position", Vector2.ZERO)
+	animationTree.set("parameters/Idle/blend_position", START_VECTOR)
+	animationTree.set("parameters/Run/blend_position", START_VECTOR)
+	animationTree.set("parameters/Attack/blend_position", START_VECTOR)
+	animationTree.set("parameters/Roll/blend_position", START_VECTOR)
 
 func _process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 	# move_and_slide uses delta automatically
@@ -46,24 +52,28 @@ func move_state(delta):
 	inputVector = inputVector.normalized()
 	
 	if inputVector != Vector2.ZERO:
+		roll_vector = inputVector
 		animationTree.set("parameters/Idle/blend_position", inputVector)
 		animationTree.set("parameters/Run/blend_position", inputVector)
 		animationTree.set("parameters/Attack/blend_position", inputVector)
+		animationTree.set("parameters/Roll/blend_position", inputVector)
 		animationState.travel("Run")
-		velocity = velocity.move_toward(inputVector * speed, acceleration * delta)
+		velocity = velocity.move_toward(inputVector * speed, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	# move_and_slide uses delta automatically
 	#velocity = move_and_slide(velocity)
-	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+		velocity = roll_vector * ROLL_SPEED
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
 func attack_state(delta):
 	# smoothly slow down
-	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	#velocity = Vector2.ZERO
 	
 	animationState.travel("Attack")
@@ -71,6 +81,13 @@ func attack_state(delta):
 	#if animationState.get_current_node() != "Attack":
 	#	state = MOVE
 
+func roll_state(delta):
+	animationState.travel("Roll")
+
 # called from animations
 func attack_anim_finished():
+	state = MOVE
+
+func roll_anim_finished():
+	velocity *= 0.8
 	state = MOVE
