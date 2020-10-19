a3766703d5e5c16f269a35315948a43afee3afc0
Jonathan Niemi <jontniemi@icloud.com>
2020-10-19 11:03:55 -0400

    Completed Part 9
    -adding attack anim and state
    -took a few creative liberties with input map and velocity handling in attack state
    -adjusted project name/description

--- 2D Action RPG Project/Scripts/Player.gd
+++ 2D Action RPG Project/Scripts/Player.gd
@@ -4,13 +4,34 @@ export var speed = 100
 const acceleration = 500
 const friction = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	# move_and_slide uses delta automatically
+	velocity = move_and_slide(velocity)
+	
+func move_state(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,6 +40,7 @@ func _physics_process(delta):
 	if inputVector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", inputVector)
 		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationTree.set("parameters/Attack/blend_position", inputVector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(inputVector * speed, acceleration * delta)
 	else:
@@ -26,4 +48,18 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
 	# move_and_slide uses delta automatically
-	velocity = move_and_slide(velocity)
+	#velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	# smoothly slow down
+	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	#velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	#if animationState.get_current_node() != "Attack":
+	#	state = MOVE
+
+func attack_anim_finished():
+	state = MOVE
